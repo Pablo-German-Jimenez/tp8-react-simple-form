dfab7c6ceca98f670bc63ae21c81e1715c2cae4e
linkin <pablogermanjimenez7@gmail.com>
2025-08-27 22:12:52 -0300

    navbar bboylinkin style done

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import FooterBboyLinkinStyle from "./components/FooterBboyLinkinStyle";
+import "./App.css";
+import NavbarBboyLinkinStyle from "./components/NavBarLinkinStyle";
+
+
+
+function App() {
+  return (
+    <>
+    <div className="app-container">
+      <NavbarBboyLinkinStyle/>
+      <main className="d-flex justify-content-center align-items-center">
+     <h1>powermoves for ever</h1>
+      </main>
+      <FooterBboyLinkinStyle/>
+        </div>
+      </>
+      );
+}
+
+export default App;
