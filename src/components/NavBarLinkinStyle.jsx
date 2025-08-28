16059fc7dab9117ec9ca797ade7d1d7c89e2fd82
linkin <pablogermanjimenez7@gmail.com>
2025-08-27 22:24:17 -0300

    component Navbar bboy linkin style done

--- src/components/NavBarLinkinStyle.jsx
+++ src/components/NavBarLinkinStyle.jsx
@@ -5,7 +5,7 @@ const NavbarBboyLinkinStyle = () => {
   return (
     <nav className="navbar-musical d-flex justify-content-center align-items-center w-100 px-4 py-2">
       <div className="d-flex justify-content-center align-items-center ">
-        <h1 className="mb-0 me-3">Formulario Simple</h1>
+        <h1 className="mb-0 me-3 text-warning">Formulario Simple</h1>
       </div>
       
       <div className="d-flex align-items-center">
@@ -14,7 +14,7 @@ const NavbarBboyLinkinStyle = () => {
           width="16"
           height="16"
           fill="currentColor"
-          className="bi bi-currency-bitcoin"
+          className="bi bi-currency-bitcoin text-warning"
           viewBox="0 0 16 16"
         >
           <path d="M5.5 13v1.25c0 .138.112.25.25.25h1a.25.25 0 0 0 .25-.25V13h.5v1.25c0 .138.112.25.25.25h1a.25.25 0 0 0 .25-.25V13h.084c1.992 0 3.416-1.033 3.416-2.82 0-1.502-1.007-2.323-2.186-2.44v-.088c.97-.242 1.683-.974 1.683-2.19C11.997 3.93 10.847 3 9.092 3H9V1.75a.25.25 0 0 0-.25-.25h-1a.25.25 0 0 0-.25.25V3h-.573V1.75a.25.25 0 0 0-.25-.25H5.75a.25.25 0 0 0-.25.25V3l-1.998.011a.25.25 0 0 0-.25.25v.989c0 .137.11.25.248.25l.755-.005a.75.75 0 0 1 .745.75v5.505a.75.75 0 0 1-.75.75l-.748.011a.25.25 0 0 0-.25.25v1c0 .138.112.25.25.25zm1.427-8.513h1.719c.906 0 1.438.498 1.438 1.312 0 .871-.575 1.362-1.877 1.362h-1.28zm0 4.051h1.84c1.137 0 1.756.58 1.756 1.524 0 .953-.626 1.45-2.158 1.45H6.927z" />
@@ -25,13 +25,13 @@ const NavbarBboyLinkinStyle = () => {
           width="16"
           height="16"
           fill="currentColor"
-          className="bi bi-currency-bitcoin"
+          className="bi bi-currency-bitcoin text-warning"
           viewBox="0 0 16 16"
         >
           <path d="M5.5 13v1.25c0 .138.112.25.25.25h1a.25.25 0 0 0 .25-.25V13h.5v1.25c0 .138.112.25.25.25h1a.25.25 0 0 0 .25-.25V13h.084c1.992 0 3.416-1.033 3.416-2.82 0-1.502-1.007-2.323-2.186-2.44v-.088c.97-.242 1.683-.974 1.683-2.19C11.997 3.93 10.847 3 9.092 3H9V1.75a.25.25 0 0 0-.25-.25h-1a.25.25 0 0 0-.25.25V3h-.573V1.75a.25.25 0 0 0-.25-.25H5.75a.25.25 0 0 0-.25.25V3l-1.998.011a.25.25 0 0 0-.25.25v.989c0 .137.11.25.248.25l.755-.005a.75.75 0 0 1 .745.75v5.505a.75.75 0 0 1-.75.75l-.748.011a.25.25 0 0 0-.25.25v1c0 .138.112.25.25.25zm1.427-8.513h1.719c.906 0 1.438.498 1.438 1.312 0 .871-.575 1.362-1.877 1.362h-1.28zm0 4.051h1.84c1.137 0 1.756.58 1.756 1.524 0 .953-.626 1.45-2.158 1.45H6.927z" />
         </svg>
-        oyLinkin♪
-        <svg className="nota-musical" viewBox="0 0 24 24" width="20" height="20">
+        <p className="text-warning my-3">oyLinkin</p>
+        <svg className="nota-musical " viewBox="0 0 24 24" width="20" height="50">
           <path fill="currentColor" d="M12 3v10.55c-.59-.34-1.27-.55-2-.55-2.21 0-4 1.79-4 4s1.79 4 4 4 4-1.79 4-4V7h4V3h-6z"/>
         </svg>
         <svg className="nota-musical" viewBox="0 0 24 24" width="20" height="20">
