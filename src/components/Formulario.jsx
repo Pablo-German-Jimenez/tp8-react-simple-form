8ed8bc40c546020be91db3028afa30c3bd2f4697
linkin <pablogermanjimenez7@gmail.com>
2025-08-29 01:27:23 -0300

    fix navbar jsx

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -5,27 +5,27 @@ function SimpleForm() {
   return (
     <Form >
       <Form.Group className="mb-3" controlId="formBasicName">
-        <Form.Label>Crew</Form.Label>
+        <Form.Label></Form.Label>
         <Form.Control type="name" placeholder="Enter your CREW BROO" />
       </Form.Group>
       <Form.Group className="mb-3" controlId="formBasicName">
-        <Form.Label>Name</Form.Label>
+        <Form.Label></Form.Label>
         <Form.Control type="name" placeholder="Enter your name" />
       </Form.Group>
       <Form.Group className="mb-3" controlId="formBasicLastname" />
-      <Form.Label>Lastname</Form.Label>
+      <Form.Label></Form.Label>
       <Form.Control type="lastname" placeholder="Enter your lastname" />
       <Form.Group className="mb-3" controlId="formBasicLastname" />
-      <Form.Label>D.N.I</Form.Label>
+      <Form.Label></Form.Label>
       <Form.Control type="dni" placeholder="Enter you D.N.I" />
       <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
+        <Form.Label></Form.Label>
         <Form.Control type="email" placeholder="Enter email" />
-        <Form.Text className="text-muted">
+        <Form.Text className="text-danger">
           We'll never share your email with anyone else.
         </Form.Text>
       </Form.Group>
-      <Button variant="primary" type="submit">
+      <Button variant="success" type="submit">
         Submit
       </Button>
     </Form>
--- src/components/NavBarLinkinStyle.jsx
+++ src/components/NavBarLinkinStyle.jsx
@@ -7,28 +7,18 @@ const NavbarBboyLinkinStyle = () => {
         <h1 className="mb-0 me-3 text-warning">Simple Form Inscription Red-Bull-Bc-One😎♪</h1>
       </div>
       
-      <div className="d-flex align-items-center">
-        <svg
+      <div className="d-flex align-items-center ">
+       <i className="bi bi-currency-bitcoin ms-5"></i>
+         <svg
           xmlns="http://www.w3.org/2000/svg"
-          width="16"
+          width="1"
           height="16"
           fill="currentColor"
-          className="bi bi-currency-bitcoin text-warning"
+          className="bi bi-currency-bitcoin "
           viewBox="0 0 16 16"
-        >
-          <path d="M5.5 13v1.25c0 .138.112.25.25.25h1a.25.25 0 0 0 .25-.25V13h.5v1.25c0 .138.112.25.25.25h1a.25.25 0 0 0 .25-.25V13h.084c1.992 0 3.416-1.033 3.416-2.82 0-1.502-1.007-2.323-2.186-2.44v-.088c.97-.242 1.683-.974 1.683-2.19C11.997 3.93 10.847 3 9.092 3H9V1.75a.25.25 0 0 0-.25-.25h-1a.25.25 0 0 0-.25.25V3h-.573V1.75a.25.25 0 0 0-.25-.25H5.75a.25.25 0 0 0-.25.25V3l-1.998.011a.25.25 0 0 0-.25.25v.989c0 .137.11.25.248.25l.755-.005a.75.75 0 0 1 .745.75v5.505a.75.75 0 0 1-.75.75l-.748.011a.25.25 0 0 0-.25.25v1c0 .138.112.25.25.25zm1.427-8.513h1.719c.906 0 1.438.498 1.438 1.312 0 .871-.575 1.362-1.877 1.362h-1.28zm0 4.051h1.84c1.137 0 1.756.58 1.756 1.524 0 .953-.626 1.45-2.158 1.45H6.927z" />
-        </svg>
-        .
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="16"
-          height="16"
-          fill="currentColor"
-          className="bi bi-currency-bitcoin text-warning"
-          viewBox="0 0 16 16"
-        >
-          <path d="M5.5 13v1.25c0 .138.112.25.25.25h1a.25.25 0 0 0 .25-.25V13h.5v1.25c0 .138.112.25.25.25h1a.25.25 0 0 0 .25-.25V13h.084c1.992 0 3.416-1.033 3.416-2.82 0-1.502-1.007-2.323-2.186-2.44v-.088c.97-.242 1.683-.974 1.683-2.19C11.997 3.93 10.847 3 9.092 3H9V1.75a.25.25 0 0 0-.25-.25h-1a.25.25 0 0 0-.25.25V3h-.573V1.75a.25.25 0 0 0-.25-.25H5.75a.25.25 0 0 0-.25.25V3l-1.998.011a.25.25 0 0 0-.25.25v.989c0 .137.11.25.248.25l.755-.005a.75.75 0 0 1 .745.75v5.505a.75.75 0 0 1-.75.75l-.748.011a.25.25 0 0 0-.25.25v1c0 .138.112.25.25.25zm1.427-8.513h1.719c.906 0 1.438.498 1.438 1.312 0 .871-.575 1.362-1.877 1.362h-1.28zm0 4.051h1.84c1.137 0 1.756.58 1.756 1.524 0 .953-.626 1.45-2.158 1.45H6.927z" />
-        </svg>
+        />
+       <div className="d-flex  align-content-baseline "><p className="text-warning my-1 ">reaker</p>°</div>
+         <i className="bi bi-currency-bitcoin "></i>
         <p className="text-warning my-3">oyLinkin</p>
         <svg className="nota-musical " viewBox="0 0 24 24" width="20" height="50">
           <path fill="currentColor" d="M12 3v10.55c-.59-.34-1.27-.55-2-.55-2.21 0-4 1.79-4 4s1.79 4 4 4 4-1.79 4-4V7h4V3h-6z"/>
@@ -36,8 +26,9 @@ const NavbarBboyLinkinStyle = () => {
         <svg className="nota-musical" viewBox="0 0 24 24" width="20" height="20">
           <path fill="currentColor" d="M12 3v10.55c-.59-.34-1.27-.55-2-.55-2.21 0-4 1.79-4 4s1.79 4 4 4 4-1.79 4-4V7h4V3h-6z"/>
         </svg>
-        <svg className="nota-musical" viewBox="0 0 24 24" width="20" height="20"/>
+        <svg className="nota-musical" viewBox="0 0 24 24" width="20" height="20">
           <path fill="currentColor" d="M12 3v10.55c-.59-.34-1.27-.55-2-.55-2.21 0-4 1.79-4 4s1.79 4 4 4 4-1.79 4-4V7h4V3h-6z"/>
+          </svg>
       </div>
     </nav>
   );
